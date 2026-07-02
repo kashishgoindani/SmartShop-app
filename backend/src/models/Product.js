6ba179c060c0c0bb714aa9f877a546ca18819fd8
Kashish <kashishgoindani266@gmail.com>
2026-07-02 22:04:04 +0500

    shopid fix

--- backend/src/models/Product.js
+++ backend/src/models/Product.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
+  shopId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   name: { type: String, required: true },
   category: { type: String },
   price: { type: Number, required: true },
