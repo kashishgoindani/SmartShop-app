0f2705f6af6abbf4b5adca99f19e290e14e32187
Kashish <kashishgoindani266@gmail.com>
2026-07-02 22:06:23 +0500

    shopid

--- backend/src/models/Sale.js
+++ backend/src/models/Sale.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const saleSchema = new mongoose.Schema({
+  shopId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   items: [{
     product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
     quantity: Number,
